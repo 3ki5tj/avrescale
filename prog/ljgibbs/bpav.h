cf39359d033d5854e908c5f871fe652cde00bf34
Cheng Zhang <3ki5tj@gmail.com>
2017-01-12 17:17:12 -0600

    add widom derivative

--- prog/ljgibbs/bpav.h
+++ prog/ljgibbs/bpav.h
@@ -1,5 +1,7 @@
 typedef struct {
   double cnt, sbp, sdbp, sbvir, sbvir2;
+  double sw, swbp;
+  double bp, dbp, bvir, w, de, dvir, bp1;
 } bpav_t;
 
 
@@ -10,12 +12,16 @@ void bpav_clear(bpav_t *bpav)
   bpav->sdbp = 0;
   bpav->sbvir = 0;
   bpav->sbvir2 = 0;
+  bpav->sw = 0;
+  bpav->swbp = 0;
 }
 
-void bpav_add(bpav_t *bpav, lj_t *lj, double beta)
+/* get pressure and derivative */
+static double lj_getbp(lj_t *lj, double beta,
+    double *bvir, double *dbp)
 {
   int n = lj->n;
-  double bp, bvir, dbp, vol = lj->vol, rho, y;
+  double bp, vol = lj->vol, rho, y;
   double irc, irc3, irc6, virtail, ytail;
 
   irc = 1/lj->rc;
@@ -23,21 +29,72 @@ void bpav_add(bpav_t *bpav, lj_t *lj, double beta)
   irc6 = irc3 * irc3;
   rho = n / vol;
   /* the tail correction for 3D */
-  virtail = 32 * M_PI * rho * n/3 * (irc6 - 1.5) * irc3;
+  virtail = 32 * M_PI * rho * n / 3 * (irc6 - 1.5) * irc3;
   ytail = 32 * M_PI * rho * n * (4*irc6 - 3) * irc3;
-  bvir = beta * (lj->vir + virtail) / (D * vol);
-  bp = n / vol + bvir;
+  *bvir = beta * (lj->vir + virtail) / (D * vol);
+  bp = n / vol + *bvir;
   y = 144 * lj->ep12 - 36 * lj->ep6 + ytail;
-  dbp = -bp/vol - beta * y / (D*D*vol*vol);
-
-  bpav->cnt += 1;
-  bpav->sbp += bp;
-  bpav->sdbp += dbp;
-  bpav->sbvir += bvir;
-  bpav->sbvir2 += bvir * bvir;
+  *dbp = -bp/vol - beta * y / (D*D*vol*vol);
+  return bp;
 }
 
-double bpav_get(bpav_t *bpav, double *dbp)
+/* energy and virial increase of adding a new particle */
+double lj_denp(lj_t *lj, double *dvir)
+{
+  double xi[D], dx[D], l = lj->l, invl = 1/l, vol = lj->vol;
+  double dr2, ir2, ir6, de, ep, ep6 = 0, ep12 = 0, eptail = 0;
+  double irc, irc3, irc6, dvirtail, detail;
+  int d, j, n = lj->n;
+
+  /* randomly choose a position */
+  for ( d = 0; d < D; d++ ) xi[d] = rand01() * lj->l;
+
+  /* compute the energy change */
+  for ( j = 0; j < n; j++ ) {
+    dr2 = lj_pbcdist2(dx, xi, lj->x[j], l, invl);
+    if ( dr2 >= lj->rc2 ) continue;
+    ir2 = 1 / dr2;
+    ir6 = ir2 * ir2 * ir2;
+    ep12 += ir6 * ir6;
+    ep6 += ir6;
+  }
+  ep6 *= 4;
+  ep12 *= 4;
+  ep = ep12 - ep6;
+  *dvir = 12 * ep12 - 6 * ep6;
+
+  /* compute the change of tail correction */
+  eptail = lj_gettail(lj->rc, (n + 1)/lj->vol, n, NULL);
+  de = ep + eptail - lj->epot_tail;
+
+  /* add the tail correction */
+  irc = 1 / lj->rc;
+  irc3 = irc * irc * irc;
+  irc6 = irc3 * irc3;
+  dvirtail = (2 * n + 1) / vol * 32 * M_PI / 3 * (irc6 - 1.5) * irc3;
+  detail = (2 * n + 1) / vol * 8 * M_PI / 9 * (irc6 - 3) * irc3;
+  *dvir += dvirtail;
+  de += detail;
+  return de;
+}
+
+void bpav_add(bpav_t *bpav, lj_t *lj, double beta)
+{
+  bpav->bp = lj_getbp(lj, beta, &bpav->bvir, &bpav->dbp);
+  bpav->de = lj_denp(lj, &bpav->dvir);
+  bpav->w = exp(-beta * bpav->de);
+  bpav->bp1 = bpav->bp + bpav->dvir * beta / (D * lj->vol);
+
+  bpav->cnt += 1;
+  bpav->sbp += bpav->bp;
+  bpav->sdbp += bpav->dbp;
+  bpav->sbvir += bpav->bvir;
+  bpav->sbvir2 += bpav->bvir * bpav->bvir;
+  bpav->sw += bpav->w;
+  bpav->swbp += bpav->w * bpav->bp1;
+}
+
+double bpav_get(bpav_t *bpav, double *dbp, double *w, double *dlnw)
 {
   double cnt = bpav->cnt, bp, bvir, var;
   if ( cnt <= 0 ) {
@@ -48,6 +105,8 @@ double bpav_get(bpav_t *bpav, double *dbp)
   bvir = bpav->sbvir / cnt;
   var = bpav->sbvir2 / cnt - bvir * bvir;
   *dbp = bpav->sdbp / cnt + var;
+  *w = bpav->sw / cnt;
+  *dlnw = bpav->swbp / bpav->sw - bp;
   return bp;
 }
 
--- prog/ljgibbs/mc.c
+++ prog/ljgibbs/mc.c
@@ -7,43 +7,11 @@
 
 int n = 108;
 int nequil = 100000;
-int nsteps = 1000000;
-double rho = 0.62;
+int nsteps = 10000000;
+double rho = 0.7;
 double tp = 1.15;
 double rcdef = 1e9; /* half-box cutoff */
 const char *fnpos = "lj.pos";
-
-
-
-/* energy increase of a new position */
-double lj_widom(lj_t *lj, double beta)
-{
-  double xi[D], dx[D], l = lj->l, invl = 1/l;
-  double dr2, ir2, ir6, de, ep, ep6 = 0, ep12 = 0, eptail = 0;
-  int d, j, n = lj->n;
-
-  /* randomly choose a position */
-  for ( d = 0; d < D; d++ ) xi[d] = rand01() * lj->l;
-
-  /* compute the energy change */
-  for ( j = 0; j < n; j++ ) {
-    dr2 = lj_pbcdist2(dx, xi, lj->x[j], l, invl);
-    if ( dr2 >= lj->rc2 ) continue;
-    ir2 = 1 / dr2;
-    ir6 = ir2 * ir2 * ir2;
-    ep12 += ir6 * ir6;
-    ep6 += ir6;
-  }
-  ep6 *= 4;
-  ep12 *= 4;
-  ep = ep12 - ep6;
-
-  /* compute the change of tail correction */
-  eptail = lj_gettail(lj->rc, (n + 1)/lj->vol, n, NULL);
-  de = ep + eptail - lj->epot_tail;
-
-  return exp(-beta * de);
-}
 
 
 
@@ -51,8 +19,8 @@ int main(void)
 {
   int t, acc;
   lj_t *lj;
-  double epsm = 0, psm = 0, accsm = 0, widsm = 0;
-  double mu, beta = 1/tp, bp, dbp, amp = 0.1/rho;
+  double epsm = 0, accsm = 0;
+  double beta = 1/tp, bp, dbp, w, dlnw, amp = 0.1/rho;
   bpav_t bpav[1];
 
   mtscramble(time(NULL));
@@ -60,6 +28,7 @@ int main(void)
   lj = lj_open(n, rho, rcdef);
   lj->dof = n * D;
   //lj->vol -= 0.5; lj_setrho(lj, n / lj->vol); 
+  //lj->vol += 0.5; lj_setrho(lj, n / lj->vol); 
   lj_energy(lj);
   /* equilibration */
   for ( t = 1; t <= nequil; t++ )
@@ -68,19 +37,15 @@ int main(void)
   for ( t = 1; t <= nsteps; t++ ) {
     acc = lj_metro(lj, amp, 1/tp);
     epsm += lj->epot;
-    psm += lj_calcp(lj, tp);
     accsm += acc;
-    widsm += lj_widom(lj, beta);
     bpav_add(bpav, lj, beta);
   }
   lj_writepos(lj, lj->x, lj->v, fnpos);
   lj_close(lj);
-  mu = tp * log((n+1)/lj->vol) - tp * log(widsm/nsteps);
-  bp = bpav_get(bpav, &dbp);
-  printf("rho %g, tp %g, ep %g, p %g, mu %g, acc %g%%\n",
-      rho, tp, epsm/nsteps/n, psm/nsteps,
-      mu, 100.*accsm/nsteps);
-  printf("vol %g, bp %g, dbp %g\n", lj->vol, bp, dbp);
+  bp = bpav_get(bpav, &dbp, &w, &dlnw);
+  printf("rho %g, tp %g, vol %g, ep %g, bp %g, dbp %g, w %g, lnw %g, dlnw %g, acc %g%%\n",
+      rho, tp, lj->vol, epsm / nsteps, bp, dbp, w, log(w), dlnw,
+      100.*accsm / nsteps);
   return 0;
 }
 
